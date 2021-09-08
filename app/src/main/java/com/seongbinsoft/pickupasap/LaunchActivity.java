1163e541604fb764eed7936a10adef03e443f2f3
devseongbin <devkim940840@gmail.com>
2021-09-08 13:40:18 +0900

    before add admob

--- app/src/main/java/com/seongbinsoft/pickupasap/LaunchActivity.java
+++ app/src/main/java/com/seongbinsoft/pickupasap/LaunchActivity.java
@@ -2,13 +2,41 @@ package com.seongbinsoft.pickupasap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class LaunchActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(r, 3000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(r);
     }
 }
---
+++ app/src/main/java/com/seongbinsoft/pickupasap/Receipt_Fragment.java
@@ -0,0 +1,14 @@
+package com.seongbinsoft.pickupasap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Receipt_Fragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt_fragment);
+    }
+}
