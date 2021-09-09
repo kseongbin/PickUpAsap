f21d9dcca8c0f65ec29207a28aaa5f1db305c171
Seongbin kim <kim940840@gmail.com>
2021-09-09 15:56:14 +0900

    day2 finish

--- app/src/main/java/com/seongbinsoft/pickupasap/ShopInfoActivity.java
+++ app/src/main/java/com/seongbinsoft/pickupasap/ShopInfoActivity.java
@@ -3,17 +3,12 @@ package com.seongbinsoft.pickupasap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class Home_Fragment extends AppCompatActivity {
-
+public class ShopInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_fragment);
-    }
-
-    public void clickKor(View view) {
+        setContentView(R.layout.activity_shop_info);
     }
 }
