28e415dc156bcab9ba9e29fd7b4dec134f5214a7
devseongbin <devkim940840@gmail.com>
2021-09-14 14:47:30 +0900

    finish attach adapter on homefrag

--- app/src/main/java/com/seongbinsoft/pickupasap/HomeFragShopListAdapter.java
+++ app/src/main/java/com/seongbinsoft/pickupasap/HomeFragShopListAdapter.java
@@ -1,22 +1,27 @@
 package com.seongbinsoft.pickupasap;
 
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
+
 
 public class HomeFragShopListAdapter extends RecyclerView.Adapter {
 
     Context context;
-    ArrayList<ShopListActivity> items;
+    ArrayList<ShopList_Item> items;
 
-    public HomeFragShopListAdapter(Context context, ArrayList<ShopListActivity> items) {
+    public HomeFragShopListAdapter(Context context, ArrayList<ShopList_Item> items) {
         this.context = context;
         this.items = items;
     }
@@ -25,15 +30,18 @@ public class HomeFragShopListAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
-        View itemView = inflater.inflate(R.layout.activity_shop_list, parent, false);
+        View itemView = inflater.inflate(R.layout.item_recyclerview_shoplist, parent, false);
         VH holder = new VH(itemView);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        VH vh = (VH) holder;
-        vh.listAdapter.notify();
+        VH vh = (VH)holder;
+        ShopList_Item item = items.get(position);
+        vh.name.setText(item.name);
+        vh.msg.setText(item.msg);
+        Glide.with(context).load(item.img).into(vh.img);
     }
 
     @Override
@@ -43,14 +51,17 @@ public class HomeFragShopListAdapter extends RecyclerView.Adapter {
 
     class VH extends RecyclerView.ViewHolder{
 
-        RecyclerView Home_Update_recyclerView;
-        ShopListAdapter listAdapter;
+        TextView name;
+        TextView msg;
+        ImageView img;
 
         public VH(@NonNull View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.tv_name);
+            msg = itemView.findViewById(R.id.tv_msg);
+            img = itemView.findViewById(R.id.iv_img);
 
-           Home_Update_recyclerView = itemView.findViewById(R.id.recyclerview);
-           listAdapter = new ShopListAdapter(context, listAdapter.items);
         }
     }
+
 }
--- app/src/main/java/com/seongbinsoft/pickupasap/Home_Fragment.java
+++ app/src/main/java/com/seongbinsoft/pickupasap/Home_Fragment.java
@@ -6,17 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Home_Fragment extends Fragment {
 
     RecyclerView rv_Update;
+    ArrayList<ShopList_Item> items = new ArrayList<>();
+    HomeFragShopListAdapter horizontalAdapter;
 
     @Nullable
     @Override
@@ -28,6 +34,7 @@ public class Home_Fragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //한식 써클이미지뷰 들어가기
         CircleImageView civ = view.findViewById(R.id.civ01);
         civ.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +44,16 @@ public class Home_Fragment extends Fragment {
             }
         });
 
+        //Horizontal RecyclerView
+        if (items!=null){
+            items.add(new ShopList_Item("마포갈매기", "장사시간 : 오전 11:00 ~ 오후 09:00", R.drawable.paris));
+            items.add(new ShopList_Item("홍콩반점", "장사시간 : 오전 10:00 ~ 오후 10:00", R.drawable.paris));
+            items.add(new ShopList_Item("하남돼지집", "장사시간 : 오전 09:00 ~ 오후 09:00", R.drawable.paris));
+        }
         rv_Update = view.findViewById(R.id.rv_update);
+        horizontalAdapter = new HomeFragShopListAdapter(getActivity(), items);
+        rv_Update.setAdapter(horizontalAdapter);
+
 
     }
 }
